08b226c5c56e58db7b8d0c3b5705bed8924f2dc7
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-03-11 00:03:55 +0100

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import robotsTxt from 'astro-robots-txt'
 import { SITE_URL } from './src/data/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), ],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: 'shiki',
