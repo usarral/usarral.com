1bb69b8846a7f3116561a34def6fd1496d6c6d33
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-03-10 22:04:44 +0000

    Format using standard

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-open-sans)", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-};
+        sans: ['var(--font-open-sans)', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
+}
