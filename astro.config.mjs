ceef1b0ceb400251373e5c267f652788fcbd9f90
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-04-26 19:42:17 +0200

    Actualizar configuración de Astro y agregar proyecto RafaelPalmero.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { SITE_URL } from './src/data/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), ],
+  integrations: [tailwind(), sitemap()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: 'shiki',
